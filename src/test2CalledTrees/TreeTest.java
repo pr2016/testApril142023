394d5c480ced88ef1ac40b0de9d5c9f1858fe5c6
Preethi Mohan <prmo2016@gmail.com>
2023-04-14 11:37:39 -0700

    initial checkin

---
+++ src/test2CalledTrees/TreeTest.java
@@ -0,0 +1,9 @@
+package test2CalledTrees;
+
+public class TreeTest 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Tree Test 1 hi");
+	}
+}
---
+++ src/test2CalledTrees/TreeTest2.java
@@ -0,0 +1,9 @@
+package test2CalledTrees;
+
+public class TreeTest2 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Tree Test 2 hi");
+	}
+}
