ae01c71ef10d17f9a25053769aae32c4b8337027
Preethi Mohan <prmo2016@gmail.com>
2023-04-20 13:51:24 -0700

    updated April 20 1:51pm

--- src/test2CalledTrees/TreeTest.java
+++ src/test2CalledTrees/TreeTest.java
@@ -4,10 +4,6 @@ public class TreeTest
 {
 	public static void main(String[] args) 
 	{
-<<<<<<< HEAD
-		System.out.println("Tree Test 1 hi change 7");
-=======
-		System.out.println("Tree Test 1 hi change 8");
->>>>>>> f55701d0d58974a31bd30e0f55b9a2989c2e743f
+		System.out.println("Tree Test 1 hi change 9");
 	}
 }
